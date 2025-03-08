38fdcafeb052da1485027a3b4968d5daf9d5eeb5
Jesdov <mord205@mail.ru>
2025-03-08 19:13:46 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-TOKEN="7804208945:AAHHuvl-rQeSXuowHWQdx7mffo0w_FU0C_U"
-URL = "postgresql+asyncpg://postgres:d142800@localhost:5432/Shop"
-admin_list = [901023256]
+TOKEN=""
+URL = ""
+admin_list = []
 ban_list = []
